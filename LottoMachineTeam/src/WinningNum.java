1c46850402c6cae80a74f7bf57338ed99fc5199e
juhatae <juhatae@218.38.137.28>
2023-05-26 11:15:18 +0900

    주하태  -  도큐멘트 작성 및 리펙토

--- LottoMachineTeam/src/WinningNum.java
+++ LottoMachineTeam/src/WinningNum.java
@@ -4,25 +4,43 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class WinningNum {
+	
 	private Map<Integer, Integer[]> winningNum;
 	
-
 	public WinningNum() {
-		
 		this.winningNum = new LinkedHashMap<>();
 	}
 
-	public Map<Integer, Integer[]> getWinningNum() {
-		return winningNum;
-	}
+	/**
+     * 당첨 번호 맵을 가져오는 메서드.
+     * 방어적 복사를 사용하여 외부 변경으로부터 보호.
+     * 작성자 : 주하태 버젼 : 1.0.0
+     * @return 당첨 번호 맵의 복사본
+     */
+    public Map<Integer, Integer[]> getWinningNum() {
+        return new LinkedHashMap<>(winningNum);
+    }
+    
+    /**
+     * 당첨 번호 맵을 설정하는 메서드.
+     * 방어적 복사를 사용하여 외부 변경으로부터 보호.
+     * 작성자 : 주하태 버젼 : 1.0.0
+     * @param winningNum 새로운 당첨 번호 맵
+     */
+    public void setWinningNum(Map<Integer, Integer[]> winningNum) {
+        this.winningNum = new LinkedHashMap<>(winningNum);
+    }
 
-	public void setWinningNum(Map<Integer, Integer[]> winningNum) {
-		this.winningNum = winningNum;
-	}
 	
+	/**
+	 * 모든 턴 번호를 가져오는 메서드.
+	 * 작성자 : 주하태 버젼 : 1.0.0
+	 * @return 턴 번호의 집합
+	 */
 	public Set<Integer> getTurn() {
-		return winningNum.keySet();
+	    return winningNum.keySet();
 	}
+	// 마지막 회차관련 메소드
 	/**
 	 * 마지막 턴을 가져오는 메서드. 
 	 * 만약 TreeSet이 비어있다면 -1을 반환한다.
@@ -35,9 +53,14 @@ public class WinningNum {
 	    return !winningNumKeySet.isEmpty() ? winningNumKeySet.last() : -1;
 	}
 	
-	@Override
-	public String toString() {
-		return "WinningNum [winningNum=" + winningNum + "]";
+	/**
+	 * 마지막 턴에 대한 당첨 번호를 가져오는 메서드.
+	 * 만약 마지막 턴이 없다면 null을 반환한다.
+	 * 작성자 : 주하태 버젼 : 1.0.0
+	 * @return 마지막 턴의 당첨 번호. 마지막 턴이 없으면 null
+	 */
+	public Integer[] getLastWinningNum() {
+	    return winningNum.get(getLastTurn());
 	}
-	
+
 }
