c1710774941d34dcd0d1bfb49ca0b824fb2c4f0e
Ximen2464 <kang_001@naver.com>
2023-05-26 17:43:53 +0900

    원본 paymenMap값을 return 추후 Paymen객체를 참조하여 선택된 번호 return하기

---
+++ LottoMachineTeam/src/PayNum.java
@@ -0,0 +1,44 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import javax.swing.JButton;
+
+public class PayNum {
+	
+//나의결제 개수의 Key값,선택번호와 자동인지확인하는 번호인 [] value값
+	private Map<Integer, Payment> paymentMap = new LinkedHashMap<>();
+	
+	/**
+	 * 외부로부터 보호하면서 읽기전용으로 접근만 가능함
+	 * 작성자 : 강청운 버전 : 1.0.0
+	 * @return
+	 */
+	public Map<Integer, Payment> getPaymentMap(){
+		
+		return new LinkedHashMap<>(paymentMap);
+		
+	}
+	
+	public Payment getSelectNum() {
+		Payment selectNum = new Payment();
+		for (int i = 0; i < selectNum.getSelectNum().length; i++) {
+			Integer integer = selectNum.getSelectNum()[i];
+			return integer;
+		}
+		
+		for(Entry<Integer, Payment> e : paymentMap.entrySet()) {
+			e.
+		}
+	}
+	
+	//각 위치에 있는 취소버튼으로 remove 적용하기
+	public void allRemove() {
+		MainFrame callBtn = new MainFrame();
+		JButton cancleBtn = callBtn.getBtn();
+		paymentMap.remove(cancleBtn);
+		//각 위치해 있는 취소버튼으로 값을 넣으면 됨
+		
+	}
+}
