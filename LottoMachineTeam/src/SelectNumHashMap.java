b325e4982e9e468369aeac2502aab73bf8ff09b2
장명근 <@DESKTOP-L6MO5E4>
2023-05-28 10:09:02 +0900

    로또 번호 HashMap으로 저장하는 class

---
+++ LottoMachineTeam/src/SelectNumHashMap.java
@@ -0,0 +1,33 @@
+import java.util.Map;
+
+public class SelectNumHashMap {
+	private Map<Integer, SelectNum> selectNumHashMap;
+
+	public Map<Integer, SelectNum> getSelectNumHashMap() {
+		return selectNumHashMap;
+	}
+	
+	/**
+	 * 추가하기
+	 * @param idx
+	 * @param selNum
+	 * @return 추가 여부 : 되었으면 true 안 되었으면 false
+	 */
+	public boolean addMap(int idx, SelectNum selNum) {
+		Integer i = new Integer(idx);
+		if (selectNumHashMap.size() < 10) {
+			selectNumHashMap.put(i, selNum);
+			return true;
+		} else {
+			return false;
+		}		
+	}
+	/**
+	 * 제거하기
+	 * @param idx
+	 */
+	public void cancleMap(int idx) {
+		Integer i = new Integer(idx);
+		selectNumHashMap.remove(i);
+	}
+}
