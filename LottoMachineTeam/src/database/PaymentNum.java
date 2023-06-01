00040ca6ed12ea2a515cc1202def396a8446cf49
juhatae <juhatae@localhost>
2023-06-01 14:00:37 +0900

    추첨하기에 paymentNum출력 가능하게

--- LottoMachineTeam/src/database/PaymentNum.java
+++ LottoMachineTeam/src/database/PaymentNum.java
@@ -3,22 +3,22 @@ package database;
 import java.util.Arrays;
 
 public class PaymentNum {
-	Integer[] selectNum;
+	Integer[] paymentNum;
 	int autoStat;
 	
 	public PaymentNum(Integer[] array, int autoValue) {
-		this.selectNum = array;
+		this.paymentNum = array;
 		this.autoStat = autoValue;
 	}
-	public Integer[] getSelectNum() {
-		return selectNum;
+	public Integer[] getPaymentNum() {
+		return paymentNum;
 	}
 	public int getAutoStat() {
 		return autoStat;
 	}
 	@Override
 	public String toString() {
-		return "PaymentNum [selectNum=" + Arrays.toString(selectNum) + ", autoStat=" + autoStat + "]";
+		return "PaymentNum [selectNum=" + Arrays.toString(paymentNum) + ", autoStat=" + autoStat + "]";
 	}
 	
 }
