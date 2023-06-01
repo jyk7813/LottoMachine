e756e4320ac9fd53502563c419930e01aa2b0c8a
juhatae <juhatae@localhost>
2023-06-01 18:01:23 +0900

    Merge branch 'master' of https://github.com/jyk7813/LottoMachine.git 10개이상 추가하기 안됨

--- LottoMachineTeam/src/Pages/BuyPage.java
+++ LottoMachineTeam/src/Pages/BuyPage.java
@@ -83,7 +83,7 @@ public class BuyPage extends JDialog {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					List<Integer> selectList = new ArrayList<Integer>();
-					if (SELECT_NUM_DATA.getSelectNumHashMap().size() < 10) {
+					if (SELECT_NUM_DATA.getSelectNumHashMap().size() < 10 && SELECT_NUM_DATA.getCount()<10) {
 						if (selectedCount.get() == 6) {
 							for (int i = 0; i < selectNum.length; i++) {
 								if (selectNum[i].isSelected()) {
@@ -103,7 +103,7 @@ public class BuyPage extends JDialog {
 						isAuto = 2;
 					if (autoCount == 0)
 						isAuto = 3;
-					if (SELECT_NUM_DATA.getSelectNumHashMap().size() < 10) {
+					if (SELECT_NUM_DATA.getSelectNumHashMap().size() < 10&& SELECT_NUM_DATA.getCount()<10) {
 						if (selectedCount.get() == 6) {
 							SELECT_NUM_DATA.addSelectNumHashMap(selectList, isAuto);
 							
--- LottoMachineTeam/src/Pages/MainPage.java
+++ LottoMachineTeam/src/Pages/MainPage.java
@@ -230,7 +230,9 @@ public class MainPage extends JFrame {
 					curruntTurnString = currentRound+"회";
 					curruntTurnLabel.setText(curruntTurnString);
 					buyPage.PAYMENT_NUM_DATA.clearData();
-					System.out.println(currentRound);	
+					buyPage.SELECT_NUM_DATA.resetCount();
+					System.out.println(currentRound);
+					
 				}
 				buyButton.setIcon(iconData.buyIcon());
 				makeLotteryButton.setIcon(iconData.makeLotteryIcon());
